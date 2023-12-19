fb673dffd334ae21d0b597b7709f1e3727a4cba8
백대홍 <bdh3620@naver.com>
2023-12-19 23:37:10 +0900

    Chore: 주석

--- iTour/Destination.swift
+++ iTour/Destination.swift
@@ -28,6 +28,6 @@ class Destination {
  새로운 프로젝트를 시작하고 가장 먼저 데이터 모델을 정의하는 이유는 일반적으로 나머지 앱흐름이 모델로 부터 진행되기 때문.
  
  그럼 여기서 @Model은 어디에 쓰느냐?
- SwiftData 데이터베이스를 써서 Destination객체를 로드 및 저장
+ SwiftData 데이터베이스를 써서 Destination객체를 로드 및 저장할때 사용한다.
  
  */
