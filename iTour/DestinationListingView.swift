0d33fb84427753a27093e2daeeb14410294c5dae
백대홍 <bdh3620@naver.com>
2023-12-26 23:38:33 +0900

    Chore:DestinationListingView 주석 추가

--- iTour/DestinationListingView.swift
+++ iTour/DestinationListingView.swift
@@ -41,3 +41,16 @@ struct DestinationListingView: View {
 #Preview {
     DestinationListingView(sort: SortDescriptor(\Destination.name))
 }
+
+
+/*
+ SwiftData 쿼리를 정렬방법
+ 1. 이름에 따라 정렬
+ - @Query(sort: \Destination.name)
+ 
+ 2.우선순위에 따라 내림차순으로 정렬
+ - @Query(sort: \Destination.priority, order: .reverse) var destinations: [Destination]
+ 
+ 3. 둘 이상의 속성을 정렬 해야할때는 배열을 사용하는 예시
+ @Query(sort: [SortDescriptor(\Destination.priority, order: .reverse), SortDescriptor(\Destination.name)]) var destinations: [Destination]
+ */
