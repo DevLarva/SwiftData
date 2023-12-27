b4a20f483798a3e03a3a71828e7abb83d4f1c767
백대홍 <bdh3620@naver.com>
2023-12-27 21:21:08 +0900

    Chore: searchable 주석

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -54,10 +54,17 @@ struct ContentView: View {
     ContentView()
 }
 
-
+//MARK: - @Query는 무엇인가?
 /*
- @Query는 무엇인가?
  SwiftData가 관리하는 모든 Destination개체를 읽는다.
  뷰가 나타나면 즉시 모든 대상을 로드하고 Destination개체가 추가, 삭제
  또는 변경될때마다 데이터베이스의 변경사항도 항시 감시한다. 그때 destinations의 속성도 업데이트한다.
  */
+
+//MARK: - searchable
+
+/*
+ - 대소문자 상관 없이 검색하기 위해서 string.contains()가 아닌, string.localizedStandardContains()
+ - https://ios-development.tistory.com/1124
+ */
+
