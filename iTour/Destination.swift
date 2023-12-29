e453e1f12e3e07e4ef98cd0d7a6983c0fb873cfd
백대홍 <bdh3620@naver.com>
2023-12-29 16:08:19 +0900

    Chore:@Relationship 주석

--- iTour/Destination.swift
+++ iTour/Destination.swift
@@ -31,4 +31,10 @@ class Destination {
  그럼 여기서 @Model은 어디에 쓰느냐?
  SwiftData 데이터베이스를 써서 Destination객체를 로드 및 저장할때 사용한다.
  
+ 
+ @Relationship?
+ Relationship은 어떤 entity가 변화될 때 다른 entity들에 미치는 변화를 표현한 것
+ - 참고 링크
+ 
+ https://medium.com/@youable.framios/swiftdata%EC%97%90%EC%84%9C-relationship-query-macro-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0-9242058ba3a6
  */
